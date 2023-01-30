6001f613b4efd4ae9a21988e3b43b3c5de5394a9
ozguryuksel <ozguryuksel21@gmail.com>
2023-01-30 23:29:56 +0300

    UserSignupPage arayüz geliştirmeleri.

--- frontend/src/UserSinupPage.js
+++ frontend/src/UserSinupPage.js
@@ -4,6 +4,24 @@ class UserSignupPage extends React.Component{
         return(
 <div>
     <h1>Sign Up</h1>
+    <div><label>User Name</label>
+    <input/>
+    </div>
+    <div>
+    <label>Display Name</label>
+    <input/>
+    </div>
+    <div>
+    <label>Password</label>
+    <input type='password'/>
+    </div>
+    <div>
+    <label>Password Repeat</label>
+    <input type='password'/>
+    </div>
+    <div>
+        <button>Sign Up</button>
+    </div>
 </div>
         );
     }
